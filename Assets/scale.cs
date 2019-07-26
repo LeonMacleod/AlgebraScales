2e1a13812ca0076076398f78abe31ca61132a75d
leon macleod <leondeanmacleod@gmail.com>
2019-07-26 12:55:16 +1200

    Added project

---
+++ Assets/scale.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scale : MonoBehaviour
+{
+
+    public List<GameObject> attachedBodies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "CanScale")
+        {
+            attachedBodies.Add(collision.gameObject);
+            collision.gameObject.AddComponent<FixedJoint>();
+            collision.gameObject.GetComponent<FixedJoint>().connectedBody = this.gameObject.GetComponent<Rigidbody>(); ;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
